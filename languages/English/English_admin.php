9a7948271efa303929e08c00cb5bae6b98f0febf
Lóna Lore <lonalore@freemail.hu>
2017-11-15 18:29:09 +0100

    Admin help block.

--- languages/English/English_admin.php
+++ languages/English/English_admin.php
@@ -19,3 +19,10 @@ define('LAN_PLUGIN_SMARTLOOK_ADMIN_09', 'The listed pages only');
 define('LAN_PLUGIN_SMARTLOOK_ADMIN_10', 'Enter one page per line. The "*" character is a wildcard. FRONTPAGE is the front page.');
 define('LAN_PLUGIN_SMARTLOOK_ADMIN_11', 'Add tracking to specific pages');
 define('LAN_PLUGIN_SMARTLOOK_ADMIN_12', 'Add tracking for specific class');
+
+define("LAN_PLUGIN_SMARTLOOK_ADMIN_HELP_01", "Help & Support");
+define("LAN_PLUGIN_SMARTLOOK_ADMIN_HELP_02", "If you think this plugin is useful and saves you a lot of work, then star it on Github! Thank you!");
+define("LAN_PLUGIN_SMARTLOOK_ADMIN_HELP_03", "Please feel free to report any bug found. Pull requests and issues are more than welcome!");
+define("LAN_PLUGIN_SMARTLOOK_ADMIN_HELP_04", "Issue");
+define("LAN_PLUGIN_SMARTLOOK_ADMIN_HELP_05", "Star");
+define("LAN_PLUGIN_SMARTLOOK_ADMIN_HELP_06", "Support on Beerpay");
